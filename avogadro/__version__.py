5f7e7ff369e93e7d5f51ce8ed415495ffc31087d
Austin Marshall <amarshall@numenta.com>
2015-08-05 14:15:48 -0700

    Bump hotfix version

--- avogadro/__version__.py
+++ avogadro/__version__.py
@@ -18,7 +18,7 @@
 #
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
-__version__ = "1.0"
+__version__ = "1.0.1"
 
 if __name__ == "__main__":
   print __version__
